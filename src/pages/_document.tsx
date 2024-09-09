46553323256465c6290b559f5934fe725d7b7c1b
Átila de Freitas <atila.jcdefreitas@protonmail.com>
2024-09-09 11:32:30 -0300

    minor changes

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,14 +3,15 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
 	return (
 		<Html lang="en">
-			<Head></Head>
-			<body>
+			<Head>
 				<title>Code Presence</title>
 				<meta
 					name="description"
 					content="At Code Presence, we bring your ideas to light"
 					key="desc"
 				/>
+			</Head>
+			<body>
 				<Main />
 				<NextScript />
 			</body>
